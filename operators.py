e533992cb87c1be2d39a401c11f5c64d6b72b600
Eng0092 <Eng.mhaa@gmail.com>
2020-05-30 23:13:58 +0300

    second changed commit

--- operators.py
+++ operators.py
@@ -15,19 +15,35 @@ print(6>=5)
 print(5>=5)
 print(int('5') <= int('6'))
 print('\n')
-print(False and False)
-print(False and True)
-print(True and False)
-print(True and True)
+
+print(False and False) #false
+print(False and True) #false
+print(True and False) #false
+print(True and True) #true
+a,b = 1,2
+print(a>b and b<a) #false
+print(a!=a and b ==b) #false
+print(a==a and b<a) #false
+print(a<b and b==b) #true
 print('\n')
-print(False or False)
-print(False or True)
-print(True or False)
-print(True or True)
+
+print(False or False) #false
+print(False or True) #true
+print(True or False) #true
+print(True or True) #true
+a,b = 4,7
+print(a>5 or b!=7) #false
+print(a!=4 or b==7) #true
+print(a>2 or b<3) #true
+print(a==4 or b==(10-3)) #true
+
+print(not False) #true
+print(not True) #false
+a,b,c = 4,2,8
+print(not a<b) #true
+print(not c!=b) #false
 print('\n')
-print(not False)
-print(not True)
-print('\n')
+
 print(5>6 or 8>2)
 print(5>6 or 8<13)
 print(5==5 and 2==(10/5))
