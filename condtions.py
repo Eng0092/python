b56047e9cb3a77d6c14d1deac85f3019029ad414
Eng0092 <Eng.mhaa@gmail.com>
2020-05-31 14:13:54 +0300

    condtions

---
+++ condtions.py
@@ -0,0 +1,6 @@
+username = input('please insert your username: ')
+password = input('please insert your password: ')
+if username == 'Mohammed' and password == '123':
+    print('Welcome to your email')
+else:
+    print('Error')
