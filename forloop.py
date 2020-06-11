27ab9e3ac3ca6cd0260936dfd2c1efadaa57a034
Eng0092 <Eng.mhaa@gmail.com>
2020-06-12 00:39:23 +0300

    forloop

---
+++ forloop.py
@@ -0,0 +1,17 @@
+subjects = ['Arabic', 'English', 'Math']
+print(subjects)
+print(subjects[0])
+print(subjects[1])
+subjects.append('History')
+print(subjects)
+print(subjects[1:3])
+print(subjects[1:4])
+print(subjects[-1])
+print(subjects[-2])
+# subjects.pop()
+# print(subjects)
+# subjects.pop(0)
+# print(subjects)
+
+for x in subjects:
+    print(x)
