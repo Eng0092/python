3632789b31d6d10a406d815a5d66e0a7bbac9ffa
Eng0092 <Eng.mhaa@gmail.com>
2020-07-17 19:20:54 +0300

    Functions

---
+++ Functions.py
@@ -0,0 +1,34 @@
+# def liner():
+#     print('-------------------------')
+
+# print("Mohammed")
+# liner()
+# print("34")
+
+def addition(num1 , num2):
+    print(int(num1) + int(num2))
+
+n1 = input('please enter the first number: ')
+n2 = input('please enter the second number: ')
+addition(n1 , n2)
+
+def subtraction(num1 , num2):
+    print(int(num1) - int(num2))
+
+n1 = input('please enter the first number: ')
+n2 = input('please enter the second number: ')
+subtraction(n1 , n2)
+
+def multiplication(num1 , num2):
+    print(int(num1) * int(num2))
+
+n1 = input('please enter the first number: ')
+n2 = input('please enter the second number: ')
+multiplication(n1 , n2)
+
+def division(num1 , num2):
+    print(int(num1) / int(num2))
+
+n1 = input('please enter the first number: ')
+n2 = input('please enter the second number: ')
+division(n1 , n2)
